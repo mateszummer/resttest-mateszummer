b6704e7f86246048c9ddd112227fae6bb8c71d76
chiadx <szummerm@gmail.com>
2018-05-02 22:04:51 +0200

    communication ok

--- src/main/java/com/codecool/language/mateszummer/RestApi/FoodAndDrinkRest.java
+++ src/main/java/com/codecool/language/mateszummer/RestApi/FoodAndDrinkRest.java
@@ -1,27 +1,17 @@
 package com.codecool.language.mateszummer.RestApi;
 
 import com.codecool.language.mateszummer.Service.FoodAndDrinkService;
+import com.codecool.language.mateszummer.dontaddthistogit;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-import netscape.javascript.JSObject;
+import com.squareup.okhttp.Request;;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import static org.springframework.data.repository.init.ResourceReader.Type.JSON;
 
 @RestController
 public class FoodAndDrinkRest {
@@ -41,19 +31,19 @@ public class FoodAndDrinkRest {
         foodAndDrinkService.addFoodAndDrink(name, price);
     }
 
-    @RequestMapping(value = "/sendMessage", method = RequestMethod.GET)
-    public void sendMessage() throws IOException {
+    @RequestMapping(value = "/sendMessage/{msg}", method = RequestMethod.GET)
+    public void sendMessage(@PathVariable("msg") String msg) throws IOException {
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
                     OkHttpClient client = new OkHttpClient();
                     JSONObject json=new JSONObject();
                     JSONObject dataJson=new JSONObject();
-                    dataJson.put("body","Hi this is sent from device to device");
-                    dataJson.put("title","dummy title");
+                    dataJson.put("body",msg);
+                    dataJson.put("title","random title");
                     json.put("notification",dataJson);
-                    json.put("to","f_ZgIqUwglI:APA91bFppEL11qSFapGRUEKLngBXb4BSwfPbcp7blT3YRrQix5yra2pb2bV908L_uECbMK78cV-V8P54nTydZH1NzSPVZykco2IRjvwzaveFm9w6zqyBAzWHOYkFKopxSnA1Xtzcpucb");
+                    json.put("to",dontaddthistogit.androidKey);
                     com.squareup.okhttp.RequestBody body = com.squareup.okhttp.RequestBody.create(JSON,json.toString());
                     Request request = new Request.Builder()
-                            .header("Authorization","key=AIzaSyA5XKkKce9Ley9Ih0vak4CkHxvVQKBXpaI")
+                            .header("Authorization", dontaddthistogit.firebaseSrvKey)
                             .url("https://fcm.googleapis.com/fcm/send")
                             .post(body)
                             .build();
